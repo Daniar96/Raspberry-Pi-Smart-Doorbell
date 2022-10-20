a0919c38b44cd588db385a48e2b5fb7896d61f0e
Dan Negru <d.negru@student.utwente.nl>
2022-10-20 12:42:17 +0200

    Update current page button color on the sidebar

--- frontend/js/general.js
+++ frontend/js/general.js
@@ -1,7 +1,15 @@
-$(function () {
-    let includes = $('[data-include]')
-    $.each(includes, function () {
-        let file = $(this).data('include')
-        $(this).load(file)
+function load_page(page) {
+    $(function () {
+        let includes = $('[data-include]')
+        $.each(includes, function () {
+            let file = $(this).data('include')
+            $(this).load(file)
+        })
     })
-})
+
+    $(window).on('load', function() {
+        let btn = document.getElementById(`${page}-btn`);
+        btn.classList.add("btn-success");
+        btn.classList.remove("btn-light");
+    });
+}
