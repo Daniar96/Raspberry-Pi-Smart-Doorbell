1c049c42757591a60abb5572f01d3d9f96c213b0
Dan Negru <d.negru@student.utwente.nl>
2022-11-04 10:10:42 +0100

    Add last image to dashboard and update toggle

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -2,7 +2,25 @@ function onOff() {
     const status = document.getElementById("alarm_status");
     if (status.textContent === 'Off') {
         status.textContent = 'On';
+        $.ajax({
+            url: `http://localhost:8080/Server_war/api/RPI/startPIR`,
+            type: "POST",
+            async: false,
+            contentType: "application/json",
+            success: function () {
+                console.log("alarm activated")
+            }
+        });
     } else {
         status.textContent = 'Off';
+        $.ajax({
+            url: `http://localhost:8080/Server_war/api/RPI/closePIR`,
+            type: "POST",
+            async: false,
+            contentType: "application/json",
+            success: function () {
+                console.log("alarm deactivated")
+            }
+        });
     }
 }
