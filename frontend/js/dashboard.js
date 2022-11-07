71fe163ad65e3a41d6c3819ad0f7c04c51437cfc
Dan Negru <d.negru@student.utwente.nl>
2022-11-07 11:24:09 +0100

    Move dashboard ajax requests to separate file

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,3 +1,4 @@
+// change alarm status
 function onOff() {
     const status = document.getElementById("alarm_status");
     if (status.textContent === 'Off') {
@@ -24,3 +25,96 @@ function onOff() {
         });
     }
 }
+
+// get alarm status
+$.ajax({
+    url: `http://localhost:8080/Server_war/api/RPI/getPIR`,
+    type: "GET",
+    async: false,
+    success: function (number) {
+        if (number == 0) {
+            document.getElementById("alarm_status").innerHTML = "Off"
+        } else {
+            document.getElementById("alarm_status").innerHTML = "On"
+            document.getElementById("customSwitches").checked = true;
+        }
+    }
+});
+
+// smoke sensor
+$.ajax({
+    url: `http://localhost:8080/Server_war/api/RPI/smokeAlert`,
+    type: "GET",
+    async: false,
+    success: function (number) {
+        if (number === 0) {
+            document.getElementById("smoke-detected").innerHTML = "No"
+        } else {
+            document.getElementById("smoke-detected").innerHTML = "Yes"
+        }
+    }
+});
+
+// flame sensor
+$.ajax({
+    url: `http://localhost:8080/Server_war/api/RPI/flameAlert`,
+    type: "GET",
+    async: false,
+    success: function (number) {
+        if (number === 0) {
+            document.getElementById("flame-detected").innerHTML = "No"
+        } else {
+            document.getElementById("flame-detected").innerHTML = "Yes"
+        }
+    }
+});
+
+// temperature sensor
+$.ajax({
+    url: `http://localhost:8080/Server_war/api/RPI/temp`,
+    type: "GET",
+    async: false,
+    success: function (response) {
+        document.getElementById("temp").innerHTML = `${response["temp"]} °C`
+    }
+});
+
+// notifications table (3 most recent)
+$.ajax({
+    url: `http://localhost:8080/Server_war/api/logs`,
+    type: "GET",
+    async: false,
+    success: function (response) {
+        let tbody = document.getElementsByTagName("tbody")[0];
+        for (let index in ["0", "1", "2"]) {
+            let td1 = document.createElement("td");
+            td1.innerHTML = response[index]["log"];
+
+            let td2 = document.createElement("td");
+            td2.innerHTML = response[index]["date"]
+
+            let tr = document.createElement("tr");
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tbody.appendChild(tr);
+        }
+    }
+});
+
+// upload last image taken
+$.ajax({
+    url: `http://localhost:8080/Server_war/api/images`,
+    type: "GET",
+    async: false,
+    success: function (response) {
+        let encoding = response[0]["encode"];
+        let image = new Image();
+        image.src = `data:image/png;base64,${encoding}`;
+        image.setAttribute("style", "max-height: 47vh;");
+        image.classList.add("d-block");
+        image.classList.add("w-100");
+
+        let row = document.getElementById("last-image");
+        row.appendChild(image);
+    }
+});
