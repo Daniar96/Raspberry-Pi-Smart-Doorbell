ee8819d56b894898dddb1ddb410dae6ea5c6245b
Dan Negru <d.negru@student.utwente.nl>
2022-11-07 12:12:49 +0100

    Update dashboard sensors info every 10 seconds

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -6,7 +6,7 @@ function onOff() {
         $.ajax({
             url: `http://localhost:8080/Server_war/api/RPI/startPIR`,
             type: "POST",
-            async: false,
+            async: true,
             contentType: "application/json",
             success: function () {
                 console.log("alarm activated")
@@ -17,7 +17,7 @@ function onOff() {
         $.ajax({
             url: `http://localhost:8080/Server_war/api/RPI/closePIR`,
             type: "POST",
-            async: false,
+            async: true,
             contentType: "application/json",
             success: function () {
                 console.log("alarm deactivated")
@@ -26,95 +26,106 @@ function onOff() {
     }
 }
 
-// get alarm status
-$.ajax({
-    url: `http://localhost:8080/Server_war/api/RPI/getPIR`,
-    type: "GET",
-    async: false,
-    success: function (number) {
-        if (number == 0) {
-            document.getElementById("alarm_status").innerHTML = "Off"
-        } else {
-            document.getElementById("alarm_status").innerHTML = "On"
-            document.getElementById("customSwitches").checked = true;
+function update_dashboard() {
+    // get alarm status
+    $.ajax({
+        url: `http://localhost:8080/Server_war/api/RPI/getPIR`,
+        type: "GET",
+        async: false,
+        success: function (number) {
+            if (number == 0) {
+                document.getElementById("alarm_status").innerHTML = "Off"
+                document.getElementById("customSwitches").checked = false;
+            } else {
+                document.getElementById("alarm_status").innerHTML = "On"
+                document.getElementById("customSwitches").checked = true;
+            }
         }
-    }
-});
+    });
 
-// smoke sensor
-$.ajax({
-    url: `http://localhost:8080/Server_war/api/RPI/smokeAlert`,
-    type: "GET",
-    async: false,
-    success: function (number) {
-        if (number === 0) {
-            document.getElementById("smoke-detected").innerHTML = "No"
-        } else {
-            document.getElementById("smoke-detected").innerHTML = "Yes"
+    // smoke sensor
+    $.ajax({
+        url: `http://localhost:8080/Server_war/api/RPI/smokeAlert`,
+        type: "GET",
+        async: false,
+        success: function (number) {
+            if (number === 0) {
+                document.getElementById("smoke-detected").innerHTML = "No"
+            } else {
+                document.getElementById("smoke-detected").innerHTML = "Yes"
+            }
         }
-    }
-});
+    });
 
-// flame sensor
-$.ajax({
-    url: `http://localhost:8080/Server_war/api/RPI/flameAlert`,
-    type: "GET",
-    async: false,
-    success: function (number) {
-        if (number === 0) {
-            document.getElementById("flame-detected").innerHTML = "No"
-        } else {
-            document.getElementById("flame-detected").innerHTML = "Yes"
+    // flame sensor
+    $.ajax({
+        url: `http://localhost:8080/Server_war/api/RPI/flameAlert`,
+        type: "GET",
+        async: false,
+        success: function (number) {
+            if (number === 0) {
+                document.getElementById("flame-detected").innerHTML = "No"
+            } else {
+                document.getElementById("flame-detected").innerHTML = "Yes"
+            }
         }
-    }
-});
+    });
 
-// temperature sensor
-$.ajax({
-    url: `http://localhost:8080/Server_war/api/RPI/temp`,
-    type: "GET",
-    async: false,
-    success: function (response) {
-        document.getElementById("temp").innerHTML = `${response["temp"]} °C`
-    }
-});
-
-// notifications table (3 most recent)
-$.ajax({
-    url: `http://localhost:8080/Server_war/api/logs`,
-    type: "GET",
-    async: false,
-    success: function (response) {
-        let tbody = document.getElementsByTagName("tbody")[0];
-        for (let index in ["0", "1", "2"]) {
-            let td1 = document.createElement("td");
-            td1.innerHTML = response[index]["log"];
-
-            let td2 = document.createElement("td");
-            td2.innerHTML = response[index]["date"]
-
-            let tr = document.createElement("tr");
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tbody.appendChild(tr);
+    // temperature sensor
+    $.ajax({
+        url: `http://localhost:8080/Server_war/api/RPI/temp`,
+        type: "GET",
+        async: false,
+        success: function (response) {
+            document.getElementById("temp").innerHTML = `${response["temp"]} °C`
         }
-    }
-});
+    });
 
-// upload last image taken
-$.ajax({
-    url: `http://localhost:8080/Server_war/api/images`,
-    type: "GET",
-    async: false,
-    success: function (response) {
-        let encoding = response[0]["encode"];
-        let image = new Image();
-        image.src = `data:image/png;base64,${encoding}`;
-        image.setAttribute("style", "max-height: 47vh;");
-        image.classList.add("d-block");
-        image.classList.add("w-100");
+    // notifications table (3 most recent)
+    $.ajax({
+        url: `http://localhost:8080/Server_war/api/logs`,
+        type: "GET",
+        async: false,
+        success: function (response) {
+            let tbody = document.getElementsByTagName("tbody")[0];
+            tbody.innerHTML = "";
+            for (let index in ["0", "1", "2"]) {
+                let td1 = document.createElement("td");
+                td1.innerHTML = response[index]["log"];
 
-        let row = document.getElementById("last-image");
-        row.appendChild(image);
-    }
+                let td2 = document.createElement("td");
+                td2.innerHTML = response[index]["date"]
+
+                let tr = document.createElement("tr");
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tbody.appendChild(tr);
+            }
+        }
+    });
+
+    // upload last image taken
+    $.ajax({
+        url: `http://localhost:8080/Server_war/api/images`,
+        type: "GET",
+        async: false,
+        success: function (response) {
+            let encoding = response[0]["encode"];
+            let image = new Image();
+            image.src = `data:image/png;base64,${encoding}`;
+            image.setAttribute("style", "max-height: 47vh;");
+            image.classList.add("d-block");
+            image.classList.add("w-100");
+
+            let row = document.getElementById("last-image");
+            row.innerHTML = "";
+            row.appendChild(image);
+        }
+    });
+
+    setTimeout(update_dashboard, 10000);
+}
+
+$(document).ready(function() {
+    setTimeout(update_dashboard, 10000);
 });
