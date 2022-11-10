d09f94709dee99d5faa5865532d5a200a0b6642f
Daniar96 <d.baialiev@student.utwente.nl>
2022-11-10 15:18:17 +0100

    More tests

--- Server/src/test/java/DatabaseTest.java
+++ Server/src/test/java/DatabaseTest.java
@@ -1,3 +1,4 @@
+import com.group17.JSONObjects.Temp;
 import com.group17.server.database.DAO;
 import jakarta.validation.constraints.AssertTrue;
 import org.junit.jupiter.api.Assertions;
@@ -10,8 +11,7 @@ import java.sql.SQLException;
  * NOTE:
  * Before running the tests make sure to change CREATE queries in database.Queries
  * to avoid messing up the database and SQL errors while running the tests.
- * It's sufficient to change a schema name to a new one.
- * Disabling setup void is also an option
+ * It's sufficient to change a schema name to a new one, or disabling setup void is also an option
  * Make sure to connect rfid to rpi_id in allowed tags table manually if you run on a new schema
  */
 public class DatabaseTest {
@@ -27,6 +27,10 @@ public class DatabaseTest {
         //DAO.registerUser(userEmail,userPass, rfid);
         //DAO.registerRpi(rpiId,rpiPass);
     }
+
+
+
+
     @Test
     void testSmoke() throws SQLException {
         DAO.smokeAlert(true,rpiId);
@@ -50,6 +54,29 @@ public class DatabaseTest {
         Assertions.assertEquals(1, DAO.getMic(rpiId));
         DAO.micAlert(false,rpiId);
         Assertions.assertEquals(0, DAO.getMic(rpiId));
+    }
+    @Test
+    void testLog() throws SQLException {
+        DAO.addLog("Test log, nothing has happened", rpiId);
+        Assertions.assertTrue(DAO.getLogs(rpiId).get(0).getLog().contains("Test"));
+    }
+
+    @Test
+    void testImage() throws SQLException{
+        DAO.addImage(rpiId,"image 22.22.2222","image encoding emulator");
+        String encoding = DAO.getImage(rpiId);
+        Assertions.assertTrue(encoding.equals("image encoding emulator"));
+    }
+
+    @Test
+    void testTemperature() throws SQLException {
+        DAO.addTemp("50","40",rpiId);
+        Temp tempinfo = DAO.getTemp(rpiId);
+        Assertions.assertEquals("50", tempinfo.getTemp());
+        Assertions.assertEquals("40", tempinfo.getHumidity());
 
     }
+
+
+
 }
